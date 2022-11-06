738389a56cfe7346d03a9ac958182df9a10272fa
Kornelius21102078 <117567502+Kornelius21102078@users.noreply.github.com>
2022-11-06 07:32:27 -0800

    Add files via upload

--- Aritmatika.java
+++ Aritmatika.java
@@ -1,13 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
-
 package com.mycompany.aritmatika;
 
 /**
  *
- * @author Acer
+ * @NIM 21102078  NAMA KORNELIUS HIBUR
  */
 public class Aritmatika {
 
--- dosen.java
+++ dosen.java
@@ -1,13 +1,8 @@
-
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.tugas_opp;
 
 /**
  *
- * @author 21102069
+ * @NIM 21102078  NAMA KORNELIUS HIBUR
  */
 public class dosen {
     private String IDDOS, NAMA, ALAMAT, MATKUL, HARI;
